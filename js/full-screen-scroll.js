146f3c733d959195895a3af9387a96d122a13e4e
Angela Moreno Prado <angelamorenoprado@MacBook-Pro-de-Angela.local>
2021-09-10 16:54:28 +0200

    Buttons for scroll

--- js/full-screen-scroll.js
+++ js/full-screen-scroll.js
@@ -5,9 +5,16 @@ let lastTime = 0;
 
 let index = 0;
 
+var button_section0 = document.getElementById("button_section0");
+var button_section1 = document.getElementById("button_section1");
+var button_section2 = document.getElementById("button_section2");
+var button_section3 = document.getElementById("button_section3");
+
+
 window.addEventListener("wheel", (e) => {
 
     const delta = e.wheelDelta;
+
     
     const currentTime = new Date().getTime();
 
@@ -19,13 +26,19 @@ window.addEventListener("wheel", (e) => {
 
     if(delta < 0)
     {
-        if(index+1 < sections.length)
+        if(parseInt(index)+1 < sections.length)
         {
+            
+            let elb = document.getElementById("button_section"+index);
+            elb.classList.remove('actual_button');
+
             index++;
             
             let elm = document.getElementById("section"+index);
+            elb = document.getElementById("button_section"+index);
             elm.scrollIntoView({behavior: "smooth"});
             e.preventDefault();
+            elb.classList.add('actual_button');
         }
         else
         {
@@ -36,10 +49,17 @@ window.addEventListener("wheel", (e) => {
     {
         if(index-1 >= 0)
         {
+
+            let elb = document.getElementById("button_section"+index);
+            elb.classList.remove('actual_button');
+
             index--;
+
             let elm = document.getElementById("section"+index);
+            elb = document.getElementById("button_section"+index);
             elm.scrollIntoView({behavior: "smooth"});
             e.preventDefault();
+            elb.classList.add('actual_button');
         }
         else
         {
@@ -49,3 +69,18 @@ window.addEventListener("wheel", (e) => {
 
     lastTime = currentTime;
 }, { passive: false });
+
+function change_section(i)
+{
+
+    let elm = document.getElementById("section"+i);
+    elm.scrollIntoView({behavior: "smooth"});
+
+    let x = document.getElementsByClassName("actual_button");
+    x[0].classList.remove("actual_button");
+
+    x = document.getElementById("button_section"+i);
+    x.classList.add("actual_button");
+
+    index = i;
+}
