aee1e032d50af38d857dd299462cff8b166bd587
Angela Moreno Prado <angelamorenoprado@MacBook-Pro-de-Angela.local>
2021-09-24 22:01:00 +0200

    Responsive sphere

--- js/sphere-technologies.js
+++ js/sphere-technologies.js
@@ -1,10 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var texts = [
-           "HTML5", "JavaScript", "PHP", "CSS3", "Python", "Java", "RESTful APIs", "Django", "Bootstrap", "WordPress", "Docker", "GitHub", "C", "MongoDB", "MySQL", "Dash", "Plotly", "PMBOK", "SCRUM", "AJAX", "JSON", "XML", "DTD", "jQuery", "PrestaShop", "PostgreSQL", "Django Volt Dashboard", "Node.js"
-       ];
-       var tc = TagCloud('#technologies-sphere', texts ,{
-         radius: 200
-
-       });
+    if(screen.width > 769)
+    {
+      var texts = [
+        "HTML5", "JavaScript", "PHP", "CSS3", "Python", "Java", "RESTful APIs", "Django", "Bootstrap", "WordPress", "Docker", "GitHub", "C", "MongoDB", "MySQL", "Dash", "Plotly", "PMBOK", "SCRUM", "AJAX", "JSON", "XML", "DTD", "jQuery", "PrestaShop", "PostgreSQL", "Django Volt Dashboard", "Node.js"
+      ];
+      var tc = TagCloud('#technologies-sphere', texts ,{
+        radius: 200
+      });
+    }
+    else
+    {
+      var texts = [
+        "HTML5", "JavaScript", "PHP", "CSS3", "Python", "Java", "RESTful APIs", "Django", "Bootstrap", "WordPress", "Docker", "GitHub", "C", "MongoDB", "MySQL", "Dash", "Plotly", "PMBOK", "SCRUM", "AJAX", "JSON", "XML", "DTD", "jQuery", "PrestaShop", "PostgreSQL", "Django Volt Dashboard", "Node.js"
+      ];
+      var tc = TagCloud('#technologies-sphere', texts ,{
+        radius: 170
+      });
+    }
+    
  });
